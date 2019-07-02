16af8b243dd89ae8190851c1f4e9f530ed698021
wxyzcctv <2846629226@qq.com>
2019-07-02 11:10:49 +0800

    通过jest完成计数器组件的快照测试

--- tests/unit/Counter.spec.js
+++ tests/unit/Counter.spec.js
@@ -1,10 +1,9 @@
-import { Mount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import Counter from "@/components/Counter.vue";
 
 describe("Counter.vue", () => {
   it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = Mount(Counter, );
-    expect(wrapper.text()).toMatch(msg);
+    const wrapper = mount(Counter);
+    expect(wrapper.html()).toMatchSnapshot();
   });
 });
